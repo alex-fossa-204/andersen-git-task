3d2e311f6db1502cc000ac2b2a8e4c50bf383911
alexFosa204 <shift.kite11@yandex.ru>
2022-02-03 10:56:11 +0300

    [add]: initial commit .gitignore src/

---
+++ src/andersen/intensive/bakulin/ApplicationRunner.java
@@ -0,0 +1,10 @@
+package andersen.intensive.bakulin;
+
+public class ApplicationRunner {
+
+    static final String HELLO_WORLD = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println(HELLO_WORLD);
+    }
+}
